61024ca510b62135af8431a7d5ec4de8ab3fb2e5
Ganesh Shinde <sindheganesh2004@gmail.com>
2023-11-01 00:03:43 +0530

    Created the profile screen and finished the project

--- lib/screens/bottom_bar.dart
+++ lib/screens/bottom_bar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:fluentui_icons/fluentui_icons.dart';
 import 'package:ticketbooking/screens/home_screen.dart';
+import 'package:ticketbooking/screens/profile_screen.dart';
 import 'package:ticketbooking/screens/search_screen.dart';
 import 'package:ticketbooking/screens/ticket_screen.dart';
 
@@ -17,7 +18,7 @@ class _BottomBarState extends State<BottomBar> {
     const HomeScreen(),
     const SearchScreen(),
     const TicketScreen(),
-    const Text("Profile"),
+    const ProfileScreen(),
   ];
 
   void _onItemTapped(int index) {
--- lib/screens/ticket_screen.dart
+++ lib/screens/ticket_screen.dart
@@ -14,8 +14,6 @@ class TicketScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final size = AppLayout.getSize(context);
-
     return Scaffold(
       backgroundColor: Styles.bgColor,
       body: Stack(
